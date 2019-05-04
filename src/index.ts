812c4f33fe55e1827fe2c86d9d16a64cb8bdf85c
SploxFox <sploxfox@gmail.com>
2019-05-03 21:35:15 -0500

    nope i was wrong

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,8 @@ document.body.appendChild(game.element);
 
 //Now let's tackle that stupid bottom bar on iOS Safari
 function hideBottomBar() {
-    window.scrollTo(0,document.body.scrollHeight);
+    window.scrollTo(0,1);
+    window.setTimeout(function() {window.scrollTo(0,document.body.scrollHeight)},100);
 }
 window.addEventListener("touchmove", hideBottomBar);
 window.addEventListener("load", hideBottomBar);
