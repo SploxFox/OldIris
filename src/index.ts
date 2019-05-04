f01aeb45a631f62d77835991e03a6f79930a9a12
SploxFox <sploxfox@gmail.com>
2019-05-03 21:23:39 -0500

    I think iOS Safari has finally been defeated

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,7 @@ document.body.appendChild(game.element);
 
 //Now let's tackle that stupid bottom bar on iOS Safari
 function hideBottomBar() {
-    window.scrollTo(0,0);
-    window.scrollTo(0,1);
+    window.scrollTo(0,document.body.scrollHeight);
 }
 window.addEventListener("touchmove", hideBottomBar);
 window.addEventListener("load", hideBottomBar);
