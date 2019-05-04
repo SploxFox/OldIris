863b43e354126c9b664366a1e8fa9c0a72ba5473
SploxFox <sploxfox@gmail.com>
2019-05-03 21:11:47 -0500

    Now ignoring node_modules + more iOS scroll stuff

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,9 @@ document.body.appendChild(game.element);
 
 
 //Now let's tackle that stupid bottom bar on iOS Safari
-window.addEventListener("touchmove", hideBottomBar);
 function hideBottomBar() {
+    window.scrollTo(0,0);
     window.scrollTo(0,1);
 }
-hideBottomBar();
+window.addEventListener("touchmove", hideBottomBar);
+window.addEventListener("load", hideBottomBar);
